51451abf2b3fc122bb0ead383ba7e4aaacc5b825
Loy Aqua <loyaqua@gmail.com>
2020-08-02 04:18:14 +0800

    Finish signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,20 @@
 class UsersController < ApplicationController
+  
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Welcome to the Toy Blog #{ @user.username }, you have successfully signed up!"
+      redirect_to articles_path
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+  
 end
